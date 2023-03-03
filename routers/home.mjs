5f943cdeee042c366bcc4f7ee51105be64090345
Yawen Li <yawenl@mun.ca>
2023-03-03 14:02:26 -0330

    jsdoc for modules

--- routers/home.mjs
+++ routers/home.mjs
@@ -1,8 +1,19 @@
 import express from 'express';
 
+/**
+ * @module homeRouter The module is to handler home router
+ */
 
 const homeRouter = express.Router();
 
+/**
+
+@function getHomePage
+@description This function is used to render the home page
+@param {object} req - The request object
+@param {object} res - The response object
+@returns {void}
+*/
 homeRouter.get('/', (req, res) => {
     res.render("home", {data: '', layout: 'layout'});  // Currently not used
 });
