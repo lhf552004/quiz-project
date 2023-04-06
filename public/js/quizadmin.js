cb41f972e457df0f89ed473144b87d3aa112fe1d
winifer _mt <winifer.mt2306@gmail.com>
2023-04-05 22:19:42 -0230

    Add quiz item page

--- public/js/quizadmin.js
+++ public/js/quizadmin.js
@@ -8,10 +8,55 @@ $(function() {
                 data: {
                     name: quizName
                 },
-                success: function(data) {
-                    console.log(data);
+                success: function() {
+                    window.location.href = '/quizitem/quiz/' + quizName + '/add-quiz-item'
                 }
             });
         }
     });
+
+    $('.answer-checkbox').on('click', (e) => {
+        const selected = $(e.target).attr('id');
+
+        $('.answer-checkbox:not(#' + selected + ')').prop('checked', false);
+    });
+    
+    const createQuizItem = function() {
+        const quizName = $('.add-item-container').data('name');
+        const question = $('.add-item-container input#question').val();
+        const optionFields = $('.option-field');
+        let optionArr = [];
+        let answer = '';
+
+        optionFields.each(function(index, elem) {
+            let option = $(elem).find('.option-text').val();
+
+            if(option.length > 0) {
+                optionArr.push(option);
+
+                if($(elem).find('.answer-checkbox').is(':checked')) {
+                    answer = option;
+                }
+            }
+        });
+
+        let dataObj = {
+            'id': 1,
+            'question': "What's the most popular fast food chain?",
+            'options': ["KFC", "McDonald's", "Wendy's", "Mary Brown's"],
+            'answer': "McDonald's"
+        };
+
+        $.ajax({
+            url: '/quizitem/' + quizName,
+            type: 'POST',
+            data: JSON.stringify(dataObj),
+            contentType: 'application/json',
+            success: function(data){
+                console.log(data);
+            }
+        });
+    }
+
+    $('.create-quiz-item-btn').on('click', createQuizItem);
 });
