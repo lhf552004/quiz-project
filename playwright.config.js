f068f7fefd048df1499f4d6154eae3f5d4e31d7d
Yawen Li <lhf552004@gmail.com>
2024-08-08 17:46:05 -0300

    change location for e2e

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./test/e2e",
+  testDir: "./e2e",
   timeout: 30000,
   use: {
     headless: true,
