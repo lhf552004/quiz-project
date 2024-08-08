99df5fbe0d6b5634a3dbfd05a7734e07b20204f6
yawen <lhf552004@gmail.com>
2024-08-08 14:53:39 -0300

    correct the name of e2e test

--- test/e2e/signup.test.js
+++ test/e2e/signup.test.js
@@ -1,7 +1,7 @@
 // tests/example.spec.mjs
 import { test, expect } from "@playwright/test";
 
-test("Login test", async ({ page, context }) => {
+test("Signup test", async ({ page, context }) => {
   await page.goto("http://localhost:3000/users/create");
   const titleLocator = await page.locator("h3");
   await expect(titleLocator).toHaveText("Create Your Account");
